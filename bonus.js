388f0f1b96343e7655d9e6cf812af2edcea12b02
Akshata Ajit Jadhav <60032303+akshatajadhav90@users.noreply.github.com>
2022-03-30 18:41:10 +0530

    Add files via upload

---
+++ bonus.js
@@ -0,0 +1,5 @@
+(function (){
+    var a ="Akshata Jadhav";
+    console.log(a);
+
+})();
---
+++ index.js
@@ -0,0 +1,13 @@
+//wrapper Function
+
+(function(exports, require, module, _filename, _dirname){
+
+    const a = require("fs");
+
+    const name="Akshata";
+
+    console.log(name);
+
+    module.exports={akdjsdhslbs}
+});
+
